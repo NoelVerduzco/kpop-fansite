0589e2e596eaa512f50b11036f709b61d7897cbd
jeanmarie <jeanmarie.jackman@gmail.com>
2023-10-04 18:08:07 -0500

    added bootstrap, bootswatch, and improved NavBar spacing, and NavLinks

--- front-end/fansite-client/src/components/NavBar.js
+++ front-end/fansite-client/src/components/NavBar.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className='navbar navbar-expand'style={{ margin: '20px', padding: '10px' }}>
+      <div className="d-flex align-items-center justify-content-between w-100">
+        <NavLink className='navbar-brand' to='/'>
+            KPop
+        </NavLink>
+        <ul className="navbar-nav" style={{ marginLeft: 'auto' }}>
+            <li className="nav-item">
+              <NavLink id="linkAbout" className='nav-link' to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink id="linkContact" className='nav-link' to="/contact">
+                Contact
+              </NavLink>
+            </li>
+        </ul>
+      </div>
     </nav>
   );
 }
