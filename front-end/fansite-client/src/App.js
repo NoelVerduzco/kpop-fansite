433487bd2bf0a7f6a198345d2d862bb0403cd845
jeanmarie <jeanmarie.jackman@gmail.com>
2023-10-04 16:32:57 -0500

    basic framework is up: Home, Nav, About, Contact - stubs

--- front-end/fansite-client/src/App.js
+++ front-end/fansite-client/src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      <h1>kpop fansite</h1>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
